41bd8ff804c23e0fd7a66c5bef282760ea031a34
DiegoAAJ <diegoaraldana@hotmail.com>
2023-04-18 00:49:16 -0500

    correcion Django4.02

--- Team_6/settings.py
+++ Team_6/settings.py
@@ -24,14 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-1&(7aps%r9$ap$t$b+-jg5i$iik$n%ebu6dkf1zg8=r-6l%yfx'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = 'RENDER' not in os.environ
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 
 INSTALLED_APPS = [
